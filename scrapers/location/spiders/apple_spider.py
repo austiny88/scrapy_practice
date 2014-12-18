73facedd71382673d1aba02567b31e4dceb7ea1d
Austin Young <austiny88@gmail.com>
2014-12-17 23:40:09 -0600

    Progress parsing store data in apple_spider to LocationItem. Completed: country, city, state, address, zipcode.

--- scrapers/location/spiders/apple_spider.py
+++ scrapers/location/spiders/apple_spider.py
@@ -15,10 +15,37 @@ class AppleSpider(scrapy.Spider):
         for path in raw_store_links:
             href = path.xpath("@href").extract()[0]
             url = u"http://www.apple.com" + href
+            #print(url)
             location_item = Request(url=url, callback=parse_store)
             location_items.append(location_item)
 
         return location_items
 
     def parse_store(self, response):
-        pass
+        item = LocationItem()
+
+        country_abrev = response.url.split('http://www.apple.com/')[1].split('/')
+        if country_abrev == 'retail':
+            country_abrev = 'us'
+        item['country'] = country_abrev
+
+        item['city'] = response.xpath("//div[@id='gallery-mapSwap']/div[1]//span[@class='locality']/text()").extract()[0]
+        item['state'] = response.xpath("//div[@id='gallery-mapSwap']/div[1]//span[@class='region']/text()").extract()[0]
+
+        raw_street_addresses = response.xpath("//div[@id='gallery-mapSwap']/div[1]//div[@class='street-address']/text()").extract()
+        street_address = []
+        for raw_street_address in raw_street_addresses:
+            street_address.append(raw_street_address.replace('\n').replace('\t'))
+
+        item['address'] = street_address
+        item['zipcode'] = response.xpath("//div[@id='gallery-mapSwap']/div[1]//span[@class='postal-code']/text()").extract()[0]
+        item['hours'] =
+        item['services'] =
+        item['store_email'] =
+        item['phone_number'] =
+        item['store_id'] =
+        item['store_url'] =
+        item['store_name'] =
+        item['weekly_ad_url'] =
+        item['store_image_url'] =
+        item['store_floor_plan_url'] =
