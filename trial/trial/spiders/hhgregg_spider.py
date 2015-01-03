ba011dac49288b7da4b8d6a99f6a80cc8b8d81ed
Austin Young <austiny88@gmail.com>
2015-01-03 14:52:02 -0600

    Added empty hhgregg spider.

---
+++ trial/trial/spiders/hhgregg_spider.py
@@ -0,0 +1,12 @@
+import scrapy
+from scrapy.http import Request
+from trial.items import ProductItem
+
+
+class HhgreggSpider(scrapy.Spider):
+    name = 'hhgregg'
+    allowed_domains = ["hhgregg.com"]
+    start_urls = ["http://www.hhgregg.com/"]
+
+    def parse(self, response):
+        pass
